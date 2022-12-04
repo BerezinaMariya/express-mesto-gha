edbad0a8ff8998c70e326c66e9ad838cb6924b66
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2022-12-04 15:33:20 +0300

    for backend

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 
-const { URL_REGEX } = require('./config/config');
+const { URL_REGEX, DEFAULT_ALLOWED_METHODS, allowedCors } = require('./config/config');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./middlewares/errors/not-found-error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -21,6 +22,26 @@ app.use(cookieParser());
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
+
+app.use(requestLogger);
 
 app.post(
   '/signin',
@@ -52,6 +73,8 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use(errorLogger);
+
 app.use(() => {
   throw new NotFoundError('Страница по указанному маршруту не найдена');
 });
@@ -59,7 +82,6 @@ app.use(() => {
 app.use(errors());
 
 app.use((err, req, res, next) => {
-  console.log(err);
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
--- config/config.js
+++ config/config.js
@@ -1,4 +1,16 @@
 const SALT_ROUND = 10;
 const URL_REGEX = /^(http|https):\/\/[w{3}.]?[\w-._~:/?#[\]@!$&'()*+,;=]#?/;
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-module.exports = { SALT_ROUND, URL_REGEX };
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+module.exports = {
+  SALT_ROUND,
+  URL_REGEX,
+  DEFAULT_ALLOWED_METHODS,
+  allowedCors,
+};
