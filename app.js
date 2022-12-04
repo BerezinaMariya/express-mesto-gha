f47c5407c95b9e7aaa2b14c4e3ee03c32e26c33c
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2022-12-04 16:56:00 +0300

    fix: cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   if (method === 'OPTIONS') {
