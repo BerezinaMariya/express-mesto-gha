7dbeec1cdd4f8c40e9ac7f95512f8f8499c5c875
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2022-12-06 15:38:37 +0300

    fix: cors again5

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,7 +14,7 @@ module.exports = ((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
