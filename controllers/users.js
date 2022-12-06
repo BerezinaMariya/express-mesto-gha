e5bb75009036205c9b6886d5b755bfd4ba8914ff
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2022-12-07 00:07:28 +0300

    fix: cors again16

--- controllers/users.js
+++ controllers/users.js
@@ -86,7 +86,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
+        sameSite: false,
       });
 
       res.send({ token });
