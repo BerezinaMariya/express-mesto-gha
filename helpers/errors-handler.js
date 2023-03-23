c3cb2ee78481d483f773dcb7e7b5b79a95e3ae90
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-03-23 16:24:39 +0300

    fix: error handler status code 500

--- helpers/errors-handler.js
+++ helpers/errors-handler.js
@@ -4,9 +4,13 @@ const {
 } = require('../config/config');
 
 module.exports.handleErrors = (err, req, res, next) => {
-  const { statusCode = INTERNAL_SERVER_ERROR_500, message } = err;
+  const { statusCode, message } = err;
 
-  res.status(statusCode).send({
+  res.status(
+    statusCode
+    ? statusCode
+    : INTERNAL_SERVER_ERROR_500
+  ).send({
     message: statusCode === INTERNAL_SERVER_ERROR_500
       ? INTERNAL_SERVER_ERROR_MESSAGE
       : message,
